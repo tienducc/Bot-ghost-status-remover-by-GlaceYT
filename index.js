5199fae56fb75bb5cd3f316d8b66707a8a84e122
Tienduc <163680285+tienducc@users.noreply.github.com>
2024-11-03 01:55:10 +0700

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to 8lang", "🎮 Playing With 8lang"];
+const statusMessages = ["🎮 Đang Chơi Với Cudien", "🎮 Đang Chơi Với Nezuko"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
