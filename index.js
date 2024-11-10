bc293cc284490437d5cc014d68e71728ec4e4d3a
Tienduc <163680285+tienducc@users.noreply.github.com>
2024-11-10 11:10:30 +0700

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["🎧 Đang Chơi Với NightT", "🎮 Playing with Tun"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
